7a1ecf3ecf3045626c0a9494af0eabe5d53bcea5
aneeshdurg <aneeshdurg17@gmail.com>
2023-04-02 14:57:22 -0500

    make deploying easier

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+set -e
 # Script for deploying the gh-pages branch
-git merge main
+git rebase main
 python3 -m build
 git add -f dist/
-git commit -m "update" --allow-empty
-git push origin gh-pages
+git commit --amend --no-edit
+git push origin gh-pages -f
