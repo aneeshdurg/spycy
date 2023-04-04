7363b3f209a2e79d1c35309576b334bf5c0b8b17
aneeshdurg <aneeshdurg17@gmail.com>
2023-04-04 09:41:57 -0500

    Implement range

--- pypher/functions/list_fns.py
+++ pypher/functions/list_fns.py
@@ -1,6 +1,9 @@
 from typing import List
 
+import numpy as np
 import pandas as pd
+
+from pypher.errors import ExecutionError
 
 
 def keys(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
@@ -16,7 +19,37 @@ def nodes(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
 
 
 def range_(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("range not implemented")
+    if len(params) not in [2, 3]:
+        raise ExecutionError("Incorrect argument count for range")
+
+    output = []
+    for i in range(len(params[0])):
+        start = params[0][i]
+        end = params[1][i]
+        if not np.issubdtype(type(start), np.integer):
+            raise ExecutionError(
+                f"TypeError::range must take ints as arguments, got {type(start)}"
+            )
+        if not np.issubdtype(type(end), np.integer):
+            raise ExecutionError(
+                f"TypeError::range must take ints as arguments, got {type(end)}"
+            )
+        step = 1
+        if len(params) == 3:
+            step = params[2][i]
+            if step == 0:
+                raise ExecutionError("NumberOutOfRange::range's step must be > 0")
+            if not np.issubdtype(type(step), np.integer):
+                raise ExecutionError(
+                    f"TypeError::range must take ints as arguments, got {type(step)}"
+                )
+
+        if start == end:
+            output.append([start])
+        else:
+            end += 1 if step > 0 else -1
+            output.append(list(range(start, end, step)))
+    return pd.Series(output)
 
 
 def relationships(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
