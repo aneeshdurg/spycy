32e50a37fa5311ea65028b7e999879fbcb166423
aneeshdurg <aneeshdurg17@gmail.com>
2023-04-04 09:43:53 -0500

    Implement numeric functions

--- pypher/functions/numeric_fns.py
+++ pypher/functions/numeric_fns.py
@@ -1,12 +1,31 @@
 from typing import List
 
+import numpy as np
 import pandas as pd
 
+from pypher.errors import ExecutionError
 
-def exists(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("exists is unimplemented")
+
+def _wrap_simple_fn(f):
+    def wrapped(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
+        if len(params) != 1:
+            raise ExecutionError(f"Invalid number of arguments")
+        return f(params[0])
+
+    return wrapped
+
+
+def rand(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
+    if len(params) != 0:
+        raise ExecutionError(f"Invalid number of arguments")
+    return pd.Series(np.random.rand(len(table)))
 
 
 fn_map = {
-    "exists": exists,
+    "abs": _wrap_simple_fn(np.abs),
+    "ceil": _wrap_simple_fn(np.ceil),
+    "floor": _wrap_simple_fn(np.floor),
+    "rand": rand,
+    "round": _wrap_simple_fn(np.round),
+    "sign": _wrap_simple_fn(np.sign),
 }
--- pypher/functions/predicate_fns.py
+++ pypher/functions/predicate_fns.py
@@ -7,35 +7,6 @@ def exists(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
     raise AssertionError("exists is unimplemented")
 
 
-def abs_(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("abs is unimplemented")
-
-
-def ceil(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("ceil is unimplemented")
-
-
-def floor(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("floor is unimplemented")
-
-
-def rand(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("rand is unimplemented")
-
-
-def round_(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("round is unimplemented")
-
-
-def sign(params: List[pd.Series], table: pd.DataFrame) -> pd.Series:
-    raise AssertionError("sign is unimplemented")
-
-
 fn_map = {
-    "abs": abs_,
-    "ceil": ceil,
-    "floor": floor,
-    "rand": rand,
-    "round": round_,
-    "sign": sign,
+    "exists": exists,
 }
